245f1fbe8be059b95310355c0f30e35a863ad261
Phil Reither <philipp.reither@gmail.com>
2017-03-07 11:37:25 +0100

    Add code from class

--- 01 - App Layout/Work Done in Class/code.js
+++ 01 - App Layout/Work Done in Class/code.js
@@ -1,10 +1,18 @@
 
 function openMenu() {
-    console.log('-- Opening the menu');
-    
+    console.log('-- Opening the menu');    
     $('nav').addClass(`open`);
 }
 
 $('main header .fa-bars').click(openMenu);
+
+
+
+function closeMenu() {
+    console.log('-- Closing the menu');    
+    $('nav').removeClass(`open`);
+}
+
+$('nav .fa-times').click(closeMenu);
 
 
