d18669f039c6da34ceb5a249bf2ab1cdaad45168
Phil Reither <philipp.reither@gmail.com>
2017-03-07 12:13:08 +0100

    Add code from class

--- 02 - Camera/Work Done in Class/code.js
+++ 02 - Camera/Work Done in Class/code.js
@@ -1,6 +1,14 @@
 
 function showImage() {
     console.log('-- Showing image');
+    
+    let reader = new FileReader();
+    
+    reader.onload = function (e) {
+        $('#preview').attr('src', e.target.result);
+    }
+    
+    reader.readAsDataURL(this.files[0]);
 }
 
 $(':file').change(showImage);
