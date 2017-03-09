0989ae17cd0bde350b2be4b912d71c5cb42c114a
Phil Reither <philipp.reither@gmail.com>
2017-03-09 08:41:56 +0100

    Add code from class

--- 03 - Geolocation/Work Done in Class/code.js
+++ 03 - Geolocation/Work Done in Class/code.js
@@ -2,7 +2,17 @@
 
 
 function updatePosition(pos) {
-    console.log('my geo location is:', pos);
+    
+    let lat = pos.coords.latitude;
+    let long = pos.coords.longitude;
+    
+    // https://maps.googleapis.com/maps/api/staticmap?center=12,24&zoom=13&size=300x300&sensor=false
+    
+    console.log('my geo location is:', lat, long);
 }
 
-navigator.geolocation.watchPosition(updatePosition);
+function onError(error) {
+    console.error('Could not get the geolocation.', error);
+}
+
+navigator.geolocation.watchPosition(updatePosition, onError);
