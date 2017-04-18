7f7e5d3a818619fb48d74a258e356e9c3ea315ff
Phil Reither <philipp.reither@gmail.com>
2017-04-18 14:33:02 +0200

    Add work done in class

--- 04 - Acceleration and Rotation Sensors/Work done in class/01 - rotation/code.js
+++ 04 - Acceleration and Rotation Sensors/Work done in class/01 - rotation/code.js
@@ -5,6 +5,13 @@ let element = document.querySelector('img');
 window.addEventListener('deviceorientation', function(event) {
     console.log(event.alpha, event.beta, event.gamma);
     
+    // android
+    element.style.transform =
+        `rotateY(${-event.gamma}deg)  
+        rotateX(${event.beta}deg) 
+        rotateZ(${event.alpha - 180}deg)`;
+    
+    // iPad
     element.style.transform =
         'rotateY(' + -event.beta + 'deg) ' + 
         'rotateX(' + -event.gamma + 'deg) ' +
