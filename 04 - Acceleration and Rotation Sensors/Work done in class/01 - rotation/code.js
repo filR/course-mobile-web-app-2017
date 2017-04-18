a2e8dd8214971fa68892c7e2f6cc745f20a3b1bf
Phil Reither <philipp.reither@gmail.com>
2017-04-18 14:31:20 +0200

    Add work done in class

--- 04 - Acceleration and Rotation Sensors/Work done in class/01 - rotation/code.js
+++ 04 - Acceleration and Rotation Sensors/Work done in class/01 - rotation/code.js
@@ -4,4 +4,9 @@ let element = document.querySelector('img');
 
 window.addEventListener('deviceorientation', function(event) {
     console.log(event.alpha, event.beta, event.gamma);
+    
+    element.style.transform =
+        'rotateY(' + -event.beta + 'deg) ' + 
+        'rotateX(' + -event.gamma + 'deg) ' +
+        'rotateZ(' + (event.alpha - 180) + 'deg)';
 });
