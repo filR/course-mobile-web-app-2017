2f79ab6b2a9dd5a0d8668eab34166ceca1ac7870
Phil Reither <philipp.reither@gmail.com>
2017-04-18 12:07:39 +0200

    Adjust rotate example

--- 04 - Acceleration and Rotation Sensors/Solution/01 - rotation/script.js
+++ 04 - Acceleration and Rotation Sensors/Solution/01 - rotation/script.js
@@ -4,8 +4,16 @@ window.addEventListener('deviceorientation', function(event) {
     // console.log(event.alpha + ' : ' + event.beta + ' : ' + event.gamma);
     
     // https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Using_device_orientation_with_3D_transforms
+
+    // android phone
+    // document.querySelector('img').style.transform =
+    //     `rotateY(${ -event.gamma }deg)
+    //      rotateX(${ event.beta }deg)
+    //      rotateZ(${ event.alpha - 180 }deg)`;
+
+    // ipad
     document.querySelector('img').style.transform =
-        `rotateY(${ -event.gamma }deg)
-         rotateX(${ event.beta }deg)
+        `rotateY(${ -event.beta }deg)
+         rotateX(${ -event.gamma }deg)
          rotateZ(${ event.alpha - 180 }deg)`;
 });
