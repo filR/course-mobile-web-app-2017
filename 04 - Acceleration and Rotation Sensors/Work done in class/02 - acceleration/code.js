f3e9f65e3244d940762a959eb085f59621005e33
Phil Reither <philipp.reither@gmail.com>
2017-04-18 15:02:51 +0200

    Add work done in class

--- 04 - Acceleration and Rotation Sensors/Work done in class/02 - acceleration/code.js
+++ 04 - Acceleration and Rotation Sensors/Work done in class/02 - acceleration/code.js
@@ -1,1 +1,11 @@
 'use strict';
+
+
+function handleMotionEvent (event) {
+    
+    console.log(event.acceleration);
+    console.log(event.accelerationIncludingGravity);
+    
+};
+
+window.addEventListener('devicemotion', handleMotionEvent)
