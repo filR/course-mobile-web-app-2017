e0b7ec9e726a7489a17eb8810707b19725e99484
Phil Reither <philipp.reither@gmail.com>
2017-04-28 14:27:27 +0200

    Add work from class

--- 05 - Gamepad/Work done in class/02 - Pong/code.js
+++ 05 - Gamepad/Work done in class/02 - Pong/code.js
@@ -1,5 +1,18 @@
+// get canvas and context
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
 
+// set canvas to full size
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+
+function drawCircle(ctx, x, y, radius, color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fill();
+}
+
+drawCircle(ctx, 100, 100, 50, 'white');
