e7b8c68ba559f7880a0beba23f137b99c0fad531
Phil Reither <philipp.reither@gmail.com>
2017-05-03 15:19:37 +0200

    Add work from class

--- 06 - Chat with Firebase/Work done in class/code.js
+++ 06 - Chat with Firebase/Work done in class/code.js
@@ -8,6 +8,37 @@ firebase.initializeApp({
 
 firebase.database().ref('messages').limitToLast(20).on('child_added', function (data) {
     
-    console.log(data, data.val());
+    // get name and message
+    let name = data.val().name;
+//    let name = data.val().name || "Anonymous";
+    let text = data.val().text;
+    
+    // empty name
+    if (!name) {
+        name = "Captain Anon";
+    }
+    
+    // <li>
+    //      <strong>Name:</strong>
+    //      Message goes here.
+    // </li>
+    
+    // create element
+    let element = $(`<li><strong>${name}</strong>${text}</li>`);
+    
+    // append element to our container
+    $('#messages').append(element);
     
 });
+
+
+
+
+
+
+
+
+
+
+
+
